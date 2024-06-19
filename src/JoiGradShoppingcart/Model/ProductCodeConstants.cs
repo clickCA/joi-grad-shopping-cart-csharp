ae26da97f66dd9c0357b6c70c107a71cf4b696ed
chanakorn-aramsak <chanakorn.aramsak@gmail.com>
2024-06-19 12:57:45 +0700

    first try on refactor

---
+++ src/JoiGradShoppingcart/Model/ProductCodeConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoiGradShoppingcart.Model
+{
+    public class ProductCodeConstants
+    {
+        public const string discount10 = "DIS_10";
+        public const string discount15 = "DIS_15";
+        public const string discount20 = "DIS_20";
+
+    }
+}
--- src/JoiGradShoppingcart/Model/ShoppingCart.cs
+++ src/JoiGradShoppingcart/Model/ShoppingCart.cs
@@ -28,56 +28,15 @@ namespace JoiGradShoppingcart.Model
 
         public Order Checkout()
         {
-            var totalPrice = 0.0;
-            var loyaltyPointsEarned = 0;
-            Dictionary<string, int> count_buy_2get1 =
-    new Dictionary<string, int>();
-            foreach (var product in _products)
-            {
-                var discount = 0.0;
-
-                if (product.ProductCode.StartsWith("DIS_10"))
-                {
-                    discount = product.Price * 0.1;
-                    loyaltyPointsEarned += (int)product.Price / 10;
-                }
-                else if (product.ProductCode.StartsWith("DIS_15"))
-                {
-                    discount = product.Price * 0.15;
-                    loyaltyPointsEarned += (int)product.Price / 15;
-                }
-                else if (product.ProductCode.StartsWith("DIS_20"))
-                {
-                    discount = product.Price * 0.20;
-                    loyaltyPointsEarned += (int)product.Price / 20;
-                }
-                else if (product.ProductCode.StartsWith("BULK_BUY_2_GET_1"))
-                {
-                    if (count_buy_2get1.ContainsKey(product.ProductCode))
-                    {
-                        count_buy_2get1[product.ProductCode] += 1;
-                    }
-                    else
-                    {
-                        count_buy_2get1.Add(product.ProductCode, 1);
-                    }
-                    if (count_buy_2get1[product.ProductCode] % 3 == 0)
-                    {
-                        discount += product.Price;
-                    }
-                    loyaltyPointsEarned += (int)product.Price / 5;
-                }
-                 else {
-                    loyaltyPointsEarned += (int) product.Price / 5;
-                }
-                totalPrice += product.Price - discount;
-            }
+            var totalPrice = CalculateTotalPrice();
+            var loyaltyPointsEarned = CalculateLoyaltyPoint();
+   
             totalPrice = totalPrice>=500 ? totalPrice*0.95 : totalPrice;
             return new Order(totalPrice, loyaltyPointsEarned);
         }
         private double CalculateTotalPrice()
         {
-           return _products.Sum(product => product.Price - );
+           return _products.Sum(product => product.Price - CalculateDiscount(product) );
         }
         private double CalculateDiscount(Product product)
         {
@@ -90,12 +49,10 @@ namespace JoiGradShoppingcart.Model
             else if (product.ProductCode.StartsWith("DIS_15"))
             {
                 discount = product.Price * 0.15;
-               
             }
             else if (product.ProductCode.StartsWith("DIS_20"))
             {
                 discount = product.Price * 0.20;
-               
             }
             else if (product.ProductCode.StartsWith("BULK_BUY_2_GET_1"))
             {
@@ -111,13 +68,8 @@ namespace JoiGradShoppingcart.Model
                 {
                     discount += product.Price;
                 }
-                loyaltyPointsEarned += (int)product.Price / 5;
+                
             }
-            else
-            {
-                loyaltyPointsEarned += (int)product.Price / 5;
-            }
-            totalPrice += product.Price - discount;
             return discount;
         }
     }
